e939f9d07bd2d42ebf845b82cc9ad31efe03fc5c
T krish <tantikrish922@gmail.com>
2026-05-14 17:00:12 +0530

    Add files via upload

---
+++ krish.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+main()
+{  
+   int a,b,c;
+   printf("Enter Number of a :");
+   scanf("%d" , &a);
+   printf("Enter Number of b :");
+   scanf("%d" , &b);
+   printf("Enter Number of c :");
+   scanf("%d" , &c);
+   
+   if
+   (a>b)
+   { if(a>c)
+     { printf("%d is max" , a );
+    
+	 }
+	 else
+	 { printf("%d is max" , c );
+	 }
+    }
+   else
+   {  if(b>c)
+       { printf("%d is max" , b );
+	   }
+	   else
+	   { printf("%d is max" , c );
+	   }
+   
+   }
+   
+}
+

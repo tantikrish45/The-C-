96cb0106904b502fe0a6eb26d7b0a5a3f9255fac
T krish <tantikrish922@gmail.com>
2026-05-17 15:39:34 +0530

    Add files via upload

---
+++ uday.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+ {
+ 	
+	char name[50];
+    int seat, total;
+    printf("Enter youer Name : ");			
+    scanf("%s" , name);
+                     	
+    printf("Enter Seats : ");
+    scanf("%d" , &seat);
+                    
+    total = seat * 200;
+    printf("Enter youer Name\n: %s", name);
+                    
+	printf("\ntotale Amount: %d", total);
+                    
+ }
+ 	
+ 	
+                     
+                     
+ 

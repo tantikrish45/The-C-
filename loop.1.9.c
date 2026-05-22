692bb428d6a3aca94ab4882efa03790b2e428e29
T krish <tantikrish922@gmail.com>
2026-05-22 18:59:32 +0530

    Add files via upload

---
+++ loop.1.9.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{ 
+   int a,c,d=1,sum=0;
+   
+   printf(" Enter your Number : ");
+   scanf("%d" , &a);
+      
+   while(a>0)   //123
+   
+   {
+   	    c=a%10;    //3
+   	    sum = sum+c; //3+2+1 
+		   d = d*c;     //    
+   	    a=a/10; //12
+   	   
+   }
+   if(d==sum)
+	    { printf("%d  valied" , d);
+    		 }
+		 else
+		  { printf(" Envalied");
+	      	  }
+}
+    

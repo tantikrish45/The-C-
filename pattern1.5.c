dc3679a3eaf7f192315c06ef09fd48f00a7e3c16
T krish <tantikrish922@gmail.com>
2026-06-03 14:11:54 +0530

    Add files via upload

---
+++ pattern1.5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{         int i,j;
+	      for(i=; i; i)
+	{
+		for(j=; j; j)
+		{
+			if()
+			{
+				if()
+				{ printf("" , );
+				}
+				else
+				{ printf("" ,);
+				}
+			}
+			else
+			{
+			    printf("" , );
+			}
+		}
+		
+		printf("\n");
+	}
+	 
+	
+}

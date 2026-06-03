943062f33cfa1f855ea2f2b1c39cd586850ef561
T krish <tantikrish922@gmail.com>
2026-06-03 14:33:22 +0530

    Add files via upload

---
+++ pattern/without pattern/pattern.1.6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{         int n=1,j,i;
+	      for(i=1; i<5; i++)
+	{
+		for(j=1; j<=5; j++)
+		{
+			prinf(" %d" , n*100);
+			n++;
+		}
+		
+		printf("\n");
+	}
+	 
+	
+}

3b40de6bbe812bb3ae1f09528b4cd78ba1213169
T krish <tantikrish922@gmail.com>
2026-06-04 16:34:40 +0530

    Add files via upload

---
+++ pattern/coustom pattern/pattern1.3.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+main()
+{
+	 int i,j,s;
+
+	for(i=5; i>=1; i--)
+	{
+		for(j=1; j<=i; j++)
+		{
+			printf("%d" , j);	
+		}
+		
+		
+		for(s=5; s>i; s--)
+		{    printf("  ");
+		}
+		
+			
+		for(j=i; j>=1; j--)
+		{	printf("%d" , j);
+		}
+		
+		printf("\n");
+		
+    }
+		for(i=1; i<=5; i++)
+		
+		{
+		
+		for(j=1; j<=i; j++)
+		{
+			printf("%d" , j);	
+		}
+		
+		
+		for(s=5; s>i; s--)
+		{    printf("  ");
+		}
+		
+			
+		for(j=i; j>=1; j--)
+		{	printf("%d" , j);
+		}
+		
+		printf("\n");	
+	}
+	
+		
+	}
+

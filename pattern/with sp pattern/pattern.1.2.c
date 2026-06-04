9b1c3fb2b96e501bf90e67876977031414314022
T krish <tantikrish922@gmail.com>
2026-06-04 10:18:38 +0530

    Add files via upload

---
+++ pattern/with sp pattern/pattern.1.2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+main()
+{
+	
+	int i,j,s;
+	
+	for(i=5; i>=1; i--)
+	{
+		for(s=15; s>i; s--)
+		{
+			printf(" ");
+		}
+		
+		for(j=i; j>=1; j--)
+		{
+			printf("%d" , j);
+		}
+		printf("\n");
+	}
+	
+	
+	
+}
+
+
+
+
+for(i=5; i>=1; i--)
+	{
+		
+		
+		for(j=1; j<=i; j++)
+		{
+			printf("%d" , j);
+		}
+		printf("\n");
+	}

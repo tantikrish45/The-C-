9618a5c46ffe69fdf727fc07c66592fcc5e5abe2
T krish <tantikrish922@gmail.com>
2026-06-06 10:22:41 +0530

    Add files via upload

---
+++ PR-4/PR4.6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{       int i,j,s;
+	for(i=1; i<=5; i++)
+	{   
+		for(j=1; j<=i; j++)
+		
+			printf("%d" , j);
+	  	for(s=5; s>i; s--)
+	  	
+	  		printf("  ");	
+		for(j=i; j>=1; j--)
+		
+			printf("%d" , j);
+		printf("\n");
+	}
+}

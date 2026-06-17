98a80c7363ba5986dae84a9256fa768922ed7884
T krish <tantikrish922@gmail.com>
2026-06-17 14:02:36 +0530

    Add files via upload

---
+++ ch-8/ch-8.3/EX/Anti-Diagonal.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+main()
+{
+    int row, col;
+
+    printf("Enter a nuber of Row : ");
+    scanf("%d", &row);
+    printf("Enter a nuber of col : ");
+    scanf("%d", &col);
+
+    int a[row][col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("Enter a[%d][%d] : ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\nArray Outout \n\n");
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+
+            if (i == 0 || j == 0 || i == row - 1 || j == col - 1)
+                printf("%d ", a[i][j]);
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}

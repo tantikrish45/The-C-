c66782ed2cf1ae11c97cd9ea155070541cd8978b
T krish <tantikrish922@gmail.com>
2026-06-22 15:44:03 +0530

    Update loop.1.6.c
    hyyyyyyyyyyyyyyyyyyyyyyyyy

--- loop/loop.1.6.c
+++ loop/loop.1.6.c
@@ -1,18 +1,27 @@
 #include<stdio.h>
 main()
 {
-	int a,rem;
+	int a,rem,b;
 	int rv=0;
 	
     printf("Enter Value of st : ");
-	scanf("%d" , &a);
-	
-	while(a!=0)
+	scanf("%d",&a);
+	b=a;
+	while(a>0)
 	{
 		 rem=a%10;
 		 rv=rv*10+rem;
 		 a= a/10;
-		 
 	}
-	printf("%d" , rv);
+	printf("%d",rv);
+
+	if(rv==b)
+	
+    {
+	 printf("\nvalied pelindrop");
+    }
+     else
+    {
+	 printf("\nEnvalied pelindrop");
+	}
 }

7d68ea5ee9472e0d532560bb54f78e84be73af76
T krish <tantikrish922@gmail.com>
2026-06-23 15:40:43 +0530

    Add files via upload

---
+++ Pelindrome.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+
+    char name[200];
+
+    printf("Enter any String : ");
+    scanf("%[^\n]", &name);
+    printf("\n");
+    printf("String : %s", name);
+
+	for (int i = 0; name[i] != '\0'; i++)
+    {
+       
+	   
+    }
+
+	// if()
+	
+    // {
+	//  printf("\nvalied pelindrome");
+    // }
+    //  else
+    // {
+	//  printf("\nEnvalied pelindrome");
+	// }
+}
